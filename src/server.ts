7a4123d9c114e3b122cdc365c274e5329a26b624
jsotoca <jsotoca@unprg.edu.pe>
2020-12-01 23:10:38 -0500

    agregando graphql al server

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,41 @@
 import express from 'express';
 import compression from 'compression';
 import cors from 'cors';
+import { IResolvers, makeExecutableSchema } from 'graphql-tools';
+import { GraphQLSchema } from 'graphql';
+import GRAPHQLHTTP, { graphqlHTTP } from 'express-graphql';
 
 const app = express();
 app.use('*',cors());
 app.use(compression());
 
-app.use('/',(req,res)=>{
-    res.send('Bienvenido al curso de GraphQL');
-});
+const typeDefs = `
+    type Query {
+        hola: String!
+        holaConNombre(nombre: String!): String!
+        holaAlCurso:String!
+    }
+`
+const resolvers:IResolvers = {
+    Query: {
+        hola(): string{
+            return 'hola mundo';
+        },
+        holaConNombre(__: void,{ nombre }): string{
+            return `hola ${nombre}`;
+        },
+        holaAlCurso(): string{
+            return 'hola al curso de GraphQL'
+        }
+    }
+}
+
+const schema:GraphQLSchema = makeExecutableSchema({typeDefs,resolvers}); 
+
+app.use('/',graphqlHTTP({
+    schema,
+    graphiql:true
+}));
 
 const PORT = 5300;
 app.listen(
